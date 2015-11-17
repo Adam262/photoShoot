3defd8431898900e0974da4d050e58f338e807f4
Adam Barcan <adam@greenhouse.io>
2015-11-17 00:08:45 -0500

    Create dropdown menu

--- main.js
+++ main.js
@@ -24,11 +24,16 @@ PhotoShoot.Photos = (function() {
 }());
 
 PhotoShoot.Events = (function() {
-  var $section = $('#fullpage > .section');
+  var $section = $('#fullpage > .section'),
+      $document = $(document),
+      $menu = $('.menu'),
+      $navbar = $('.navbar');
 
   function init() {
+    globalPreventDefault();
     appendSlides(PhotoShoot.Photos);
     initFullpage();
+    initDropit();
   }
 
   function imageTag(photo) {
@@ -46,16 +51,40 @@ PhotoShoot.Events = (function() {
     });
   }
 
+  function globalPreventDefault() {
+    $document.on('click', 'a', function(event) {
+      event.preventDefault();
+    });
+  }
+
   function initFullpage() {
-    $('#fullpage').fullpage(
-      {
-        resize: false,
-        scrollingSpeed: 2000,
-        easing: 'easeOutSine',
-        controlArrows: false,
-        afterLoad: setInterval(function(){ $.fn.fullpage.moveSlideRight() }, 2000)
-      }
-    );
+    $('#fullpage').fullpage({
+      resize: false,
+      scrollingSpeed: 2000,
+      easing: 'easeOutSine',
+      controlArrows: false,
+      afterLoad: setInterval(function(){ $.fn.fullpage.moveSlideRight() }, 2000)
+    });
+  }
+
+  function initDropit() {
+    $menu.dropit({
+      action: 'mouseenter',
+      beforeShow: beforeDropitShow,
+      afterHide: afterDropitHide
+    });
+  }
+
+  function beforeDropitShow() {
+    $navbar.css({
+      height: '10%'
+    });
+  }
+
+  function afterDropitHide() {
+    $navbar.css({
+      height: '3%'
+    });
   }
 
   return { init: init }; 
